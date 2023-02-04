4fe1caca8581b9fe26d5f673b36926aa389a5c80
m.bazrafkn <mhrn.bzrafkn.dev@gmail.com>
2023-02-04 03:12:18 -0800

    refactor and add contact us to informations controller completed

--- src/Shop.RestApi/Controllers/InfoController.cs
+++ src/Shop.RestApi/Controllers/InfoController.cs
@@ -4,15 +4,68 @@ namespace Shop.RestApi.Controllers
 {
     [ApiVersion("1.0")]
     [ApiController]
-    [Route("api/v{version:apiVersion}/informations")]
+    [Route("api/v{version:apiVersion}/infos")]
     public class InfoController : ControllerBase
     {
-        [HttpGet]
-        public string Informations()
+        [HttpGet("introduction")]
+        public GetIntroductionDto Informations()
         {
-            var informations = "This Is Some Information About This API. \n" +
-                               "we have many useful apis for creating many powerfull websites";
-            return informations;
+            var introduction = new GetIntroductionDto(
+                "Shop Api For Your Shop Projects",
+                "This Is Some Information About This API.",
+                "we have many useful apis for creating many powerfull websites.");
+
+            return introduction;
         }
+
+        [HttpGet("contact-us")]
+        public GetContactUsDto ContactUs()
+        {
+            var ContactUs = new GetContactUsDto(
+                "MHRN",
+                "+9308894481",
+                "Shiraz - Nasr blvd - Feyz st - Hamed Building",
+                "https://www.mhrn.com",
+                "mhrn.bzrafkn.dev@gmail.com");
+
+            return ContactUs;
+        }
+    }
+
+    public class GetIntroductionDto
+    {
+        public GetIntroductionDto(string title, string description, string content)
+        {
+            Title = title;
+            Description = description;
+            Content = content;
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class GetContactUsDto
+    {
+        public GetContactUsDto(
+            string companyName,
+            string phoneNumber,
+            string address,
+            string webSite,
+            string email)
+        {
+            CompanyName = companyName;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            WebSite = webSite;
+            Email = email;
+        }
+
+        public string CompanyName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string WebSite { get; set; }
+        public string Email { get; set; }
     }
 }
